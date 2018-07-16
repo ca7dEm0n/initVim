52bb47a15a5ba6e35f5a1beb7d7750fc6b59eb13
10623 <a-ca7@addcn.com>
2018-07-16 16:51:24 +0800

    fff

--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo -n "在当前系统所有用户初始化VIM? (Y/n) " && read int && echo ${int} |grep -iq "y" && git clone https://github.com/ca7dEm0n/initVim.git && cd initVim && sh installVim.sh && echo "初始化完成"
